175641882f6471bfd32201748f00a39d6dd7b68f
Frank H Mcmanus <frank@thefrankharvey.com>
2017-11-06 04:55:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ function whisper(string) {
   return string.toLowerCase();
 }
 
+function logWhisper(string) {
+  
+}
+
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
   return ("I can't hear you!");
