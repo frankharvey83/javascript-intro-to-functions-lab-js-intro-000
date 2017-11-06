b61d799dcd82dc1ded3fcb9cbbfab52402b17da4
Frank H Mcmanus <frank@thefrankharvey.com>
2017-11-06 04:49:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ function whisper(string) {
   return string.toLowerCase();
 }
 
-function logshout(string) {
-  return console.log(string.toUpperCase());
+function sayHiToGrandma(string) {
+  if (string.toLowerCase()) {
+  return ("I can't hear you.");
+} else if (string.toUpperCase())  {
+  return ("YES INDEED!");
+} else if (string === "I love you, Grandma.") {
+  return ("I love you, too.");
 }
+  }
